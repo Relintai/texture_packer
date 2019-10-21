5c4aa02eb8c537a5053f7917fa0ba0f9a07a8667
Relintai <relintai@gmail.com>
2019-10-21 12:18:31 +0200

    Little work.

--- merge_texture.h
+++ merge_texture.h
@@ -13,6 +13,9 @@ class MergeTexture : public ImageTexture {
 	GDCLASS(MergeTexture, ImageTexture);
 
 public:
+	void add(Ref<Texture> texture);
+	void get_texture(int index);
+	int get_texture_count();
 	void merge();
 
 	String test();
@@ -31,6 +34,7 @@ protected:
 	static void _bind_methods();
 
 private:
+	bool _keep_original_atlases;
 	std::vector<bin> _bins;
 	Vector<MergeTextureEntry> _entries;
 };
